a728409af01e419e3a34914fd37826ee5593b5b2
ZainAalabidin <zynly9303@gmail.com>
2023-08-11 08:45:55 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * hello
- * description
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
