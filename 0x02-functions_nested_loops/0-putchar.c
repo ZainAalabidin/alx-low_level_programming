03757fe8fb9626827992bcf73a2742917a6fc0c3
ZainAalabidin <zynly9303@gmail.com>
2023-08-16 10:42:18 -0700

    m

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main -print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
+int main(void)
+{
+_putchr('_');
+_putchr('p');
+_putchr('u');
+_putchr('t');
+_putchr('c');
+_putchr('h');
+_putchr('a');
+_putchr('r');
+_putchr('\n');
+return (0);
+}
