31c693b75a57efd9be64a322b3ca70281394b927
ZainAalabidin <zynly9303@gmail.com>
2023-08-16 18:34:39 -0700

    m

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,16 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
-	} else if (n == 0)
+	}
+       	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
-	} if (n < 0)
+	}
+       	if (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 		return (-1);
 	}
 }
-
