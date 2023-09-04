36fc1b547a6025a1a7a8eb15122222d5d287fe72
ZainAalabidin <zynly9303@gmail.com>
2023-09-04 05:25:23 -0700

    m

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,12 @@
  * main - print the name of the program
  * @argc: argument count
  * @argv: srgument vector
- * return: always 0
+ * Return: always 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char const *argv[])
 {
+	(void)argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
