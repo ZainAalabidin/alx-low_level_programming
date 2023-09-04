82f0a6d5ca56304ed1925ada117c2dcc24768af6
ZainAalabidin <zynly9303@gmail.com>
2023-09-04 05:23:44 -0700

    m

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: srgument vector
  * return: always 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
