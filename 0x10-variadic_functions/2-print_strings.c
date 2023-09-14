a31f3c03bf455556cdf9a3e4f431b0c09bf31e0f
ZainAalabidin <zynly9303@gmail.com>
2023-09-14 11:52:20 -0700

    m

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 
 /**
- * print_numbers - Prints numbers, followed by a new line.
+ * print_strings - Prints numbers, followed by a new line.
  * @separator: The string to be printed between numbers.
  * @n: The number of integers passed to the function.
  * @...: A variable number of numbers to be printed.
