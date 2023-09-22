b1857663f18821679d041a2a708c8306467c321e
ZainAalabidin <zynly9303@gmail.com>
2023-09-22 16:25:10 -0700

    singly linked list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@ void first(void)__attribute__((constructor));
  * first - prints a sentence before the main
  * functiom is executed
  */
-void frist(void )
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
