98c644832381d06b823cb93d915152bf1844d3f4
ZainAlabidin <zynly9303@gmail.com>
2024-05-21 18:35:06 +0300

    ok

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,7 +11,7 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	int i = 0;
 
 	if (array == NULL)
 		return (-1);
@@ -22,7 +22,7 @@ int linear_search(int *array, size_t size, int value)
 		if (value == array[i])
 		{
 			return (i);
-		}
+		};
 	}
 	return (-1);
 }
