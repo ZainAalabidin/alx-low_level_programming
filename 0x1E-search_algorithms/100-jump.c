03fe7d9ba5f89b7ffbaccee01d45472734a93a26
ZainAlabidin <zynly9303@gmail.com>
2024-05-25 18:15:43 +0300

    ok

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,7 +14,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t i;
 	size_t jump = sqrt(size);
 
-	if (!array || array == NULL)
+	if (!array || size == 0)
 		return (-1);
 
 	for (i = 0; i < size; i = i + jump)
